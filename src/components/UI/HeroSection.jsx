b78c9aba12659b22af786cf927d83e4a76dac343
raj-koshta <rajkoshta.rk1@gmail.com>
2025-03-23 12:36:55 +0530

    creating about page

---
+++ src/components/UI/HeroSection.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaLongArrowAltRight } from 'react-icons/fa'
+import img from "/images/world.png"
+
+const HeroSection = () => {
+  return (
+    <main className='hero-section main'>
+      <div className='container grid grid-two-cols'>
+        <div className='hero-content'>
+          <h1 className='heading-xl'>
+            Explore the World, One Country at a time.
+          </h1>
+          <p className='paragraph'>
+            Discover the history, calture, and beauty of every nation. 
+            Sort, search, and filter through countries to find the details you.
+          </p>
+          <button className='btn btn-darken btn-inline bg-white-box'>
+            Start Exploring <FaLongArrowAltRight />
+          </button>
+        </div>
+        <div className='hero-image'>
+          <img src={img} 
+            className='banner-image'
+            alt='World Beauty' />
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default HeroSection
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import countires from "../api/countries.json"
 
 const About = () => {
+
+
+
   return (
-    <h1>About</h1>
+    <section className='section-about container'>
+      <h2 className='container-title'>
+        Here are the Interesting Facts
+        <br />
+        we're proud of
+      </h2>
+      <div className='gradient-cards'>
+
+        {
+          countires.map((country) => {
+
+            const {id, countryName, capital, population, interestingFact} = country;
+
+            return <div className='card' key={id}>
+              <div className='container-card bg-blue-box'>
+                <p className='card-title'>
+                  {countryName}
+                </p>
+                <p>
+                  <span className='card-description'>Capital: </span>
+                  {capital}
+                </p>
+                <p>
+                  <span className='card-description'>Population: </span>
+                  {population}
+                </p>
+                <p>
+                  <span className='card-description'>Interesting Fact: </span>
+                  {interestingFact}
+                </p>
+              </div>
+            </div>
+          })
+        }
+
+        <div className='card'>
+          <div className='container-card bg-blue-box'>
+            <p className='card-title'>
+              India
+            </p>
+            <p>
+              <span className='card-description'>Capital: </span>
+              Delhi
+            </p>
+            <p>
+              <span className='card-description'>Population: </span>
+              81651651651
+            </p>
+            <p>
+              <span className='card-description'>Interesting Fact: </span>
+              We Love India
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
