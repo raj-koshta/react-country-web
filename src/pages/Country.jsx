a69d6c0037812adf62437a68bf6a1ec821280d5e
raj-koshta <rajkoshta.rk1@gmail.com>
2025-03-25 20:03:34 +0530

    creating CountryCard

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState, useTransition } from 'react'
+import { getCountryData } from '../api/postApi';
+import Loader from '../components/UI/Loader';
+import CountryCard from '../components/layout/CountryCard';
 
 const Country = () => {
+
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([])
+
+  useEffect(()=>{
+    try{
+      startTransition(async ()=>{
+        const res = await getCountryData();
+        setCountries(res.data);
+      })
+    } catch(error){
+      console.log(error);
+    }
+  },[])
+
+  if (isPending) return <Loader />
+
   return (
-    <h1>Country</h1>
+    <section className='country-section'>
+      <ul className='grid grid-four-cols'>
+        {
+          countries.map((CurCountry, index)=>{
+            return <CountryCard country={CurCountry} key={index} />
+          })
+        }
+      </ul>
+    </section>
   )
 }
 
